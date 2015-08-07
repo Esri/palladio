0ea90affc4e57829676122bd7490eb46016baf3e
Simon Haegler <shaegler@esri.com>
2015-08-07 17:51:12 +0000

    set spare param to rule attribute default values

--- prt4houdini/client/src/SOP_prt4houdini.cpp
+++ prt4houdini/client/src/SOP_prt4houdini.cpp
@@ -51,13 +51,15 @@
 namespace {
 
 // global prt settings
-const prt::LogLevel PRT_LOG_LEVEL		= prt::LOG_DEBUG;
+const prt::LogLevel PRT_LOG_LEVEL		= prt::LOG_WARNING;
+//const prt::LogLevel PRT_LOG_LEVEL		= prt::LOG_DEBUG;
 const char*		PRT_LIB_SUBDIR			= "prtlib";
 const char*		FILE_FLEXNET_LIB		= "flexnet_prt";
 const wchar_t*	FILE_CGA_ERROR			= L"CGAErrors.txt";
 const wchar_t*	FILE_CGA_PRINT			= L"CGAPrint.txt";
 
 // some encoder IDs
+const wchar_t*	ENCODER_ID_CGA_EVALATTR	= L"com.esri.prt.core.AttributeEvalEncoder";
 const wchar_t*	ENCODER_ID_CGA_ERROR	= L"com.esri.prt.core.CGAErrorEncoder";
 const wchar_t*	ENCODER_ID_CGA_PRINT	= L"com.esri.prt.core.CGAPrintEncoder";
 const wchar_t*	ENCODER_ID_HOUDINI		= L"HoudiniEncoder";
@@ -77,7 +79,7 @@ const bool DBG = false;
 
 class HoudiniGeometry : public HoudiniCallbacks {
 public:
-	HoudiniGeometry(GU_Detail* gdp) : mDetail(gdp) { }
+	HoudiniGeometry(GU_Detail* gdp, prt::AttributeMapBuilder* eab = nullptr) : mDetail(gdp), mEvalAttrBuilder(eab) { }
 
 protected:
 	virtual void setVertices(double* vtx, size_t size) {
@@ -147,12 +149,21 @@ protected:
 		return prt::STATUS_OK;
 	}
 	virtual prt::Status attrBool(size_t isIndex, int32_t shapeID, const wchar_t* key, bool value) {
+		if (mEvalAttrBuilder != nullptr) {
+			mEvalAttrBuilder->setBool(key, value);
+		}
 		return prt::STATUS_OK;
 	}
 	virtual prt::Status attrFloat(size_t isIndex, int32_t shapeID, const wchar_t* key, double value) {
+		if (mEvalAttrBuilder != nullptr) {
+			mEvalAttrBuilder->setFloat(key, value);
+		}
 		return prt::STATUS_OK;
 	}
 	virtual prt::Status attrString(size_t isIndex, int32_t shapeID, const wchar_t* key, const wchar_t* value) {
+		if (mEvalAttrBuilder != nullptr) {
+			mEvalAttrBuilder->setString(key, value);
+		}
 		return prt::STATUS_OK;
 	}
 
@@ -162,6 +173,8 @@ private:
 	std::vector<UT_Vector3> mPoints;
 	std::vector<int> mIndices;
 	GEO_PolyCounts mPolyCounts;
+
+	prt::AttributeMapBuilder* const mEvalAttrBuilder;
 };
 
 typedef std::vector<const prt::InitialShape*> InitialShapePtrVector;
@@ -169,13 +182,18 @@ typedef std::vector<const prt::AttributeMap*> AttributeMapPtrVector;
 
 struct InitialShapeContext {
 	InitialShapeContext(OP_Context& ctx, GU_Detail& detail) : mContext(ctx) {
+		LOG_DBG << "-- creating intial shape context";
+
 		//mAMB = prt::AttributeMapBuilder::create();
 		mISB = prt::InitialShapeBuilder::create();
 
 		// collect all primitive attributes
 		for (GA_AttributeDict::iterator it = detail.getAttributeDict(GA_ATTRIB_PRIMITIVE).begin(GA_SCOPE_PUBLIC); !it.atEnd(); ++it) {
 			mPrimitiveAttributes.push_back(GA_ROAttributeRef(it.attrib()));
+			LOG_DBG << "    prim attr: " << mPrimitiveAttributes.back()->getName();
 		}
+		LOG_DBG << "    got primitive attribute count = " << mPrimitiveAttributes.size();
+
 	}
 
 	~InitialShapeContext() {
@@ -249,13 +267,8 @@ void newSopOperator(OP_OperatorTable *table) {
 	if (prtLicHandle == 0 || status != prt::STATUS_OK)
 		return;
 
-	table->addOperator(new OP_Operator("prt4houdini",
-			"prt4houdini",
-			prt4hdn::SOP_PRT::myConstructor,
-			NODE_PARAM_TEMPLATES,
-			1,
-			1,
-			0)
+	table->addOperator(new OP_Operator("prt4houdini", "prt4houdini", prt4hdn::SOP_PRT::myConstructor,
+			NODE_PARAM_TEMPLATES, 1, 1, 0)
 	);
 }
 
@@ -317,6 +330,9 @@ void SOP_PRT::createInitialShape(const GA_Group* group, void* ctx) {
 	static const bool DBG = true;
 
 	InitialShapeContext* isc = static_cast<InitialShapeContext*>(ctx);
+	if (DBG) {
+		LOG_DBG << "-- creating initial shape geo from group " << group->getName(); // << ": vtx = " << vtx << ", idx = " << idx << ", faceCounts = " << faceCounts;
+	}
 
 	// preset attribute builder with node values
 	// TODO for dynamic UI
@@ -344,7 +360,9 @@ void SOP_PRT::createInitialShape(const GA_Group* group, void* ctx) {
 		vtx.push_back(static_cast<double>(p.z()));
 	}
 
-	boost::dynamic_bitset<> setAttributes(isc->mPrimitiveAttributes.size(), 1);
+	// loop over all polygons in the primitive group
+	// in case of multipoly initial shapes, attr on first one wins
+	boost::dynamic_bitset<> setAttributes(isc->mPrimitiveAttributes.size());
 	GA_Primitive* prim = nullptr;
 	GA_FOR_ALL_GROUP_PRIMITIVES(gdp, static_cast<const GA_PrimitiveGroup*>(group), prim) {
 		GU_PrimPoly* face = static_cast<GU_PrimPoly*>(prim);
@@ -354,25 +372,23 @@ void SOP_PRT::createInitialShape(const GA_Group* group, void* ctx) {
 		}
 		faceCounts.push_back(static_cast<uint32_t>(face->getVertexCount()));
 
-		// extract attr
-		size_t ai = setAttributes.find_first();
-		while (ai < isc->mPrimitiveAttributes.size() && ai != boost::dynamic_bitset<>::npos) {
-			if (DBG) LOG_DBG << "next unset attribute: " << ai;
-			const GA_ROAttributeRef& ar = isc->mPrimitiveAttributes[ai];
-			if (ar.isFloat() || ar.isInt()) {
-				double v = prim->getValue<double>(ar);
-				if (DBG) LOG_DBG << "   attrib " << ar->getName() << " = " << v;
-				std::wstring wn = utils::toUTF16FromOSNarrow(ar->getName());
-				mAttributeSource->setFloat(wn.c_str(), v);
-			} else if (ar.isString()) {
-				// TODO
+		// extract primitive attributes
+		for (size_t ai = 0; ai < isc->mPrimitiveAttributes.size(); ai++) {
+			if (!setAttributes[ai]) {
+				const GA_ROAttributeRef& ar = isc->mPrimitiveAttributes[ai];
+				if (ar.isFloat() || ar.isInt()) {
+					double v = prim->getValue<double>(ar);
+					if (DBG) LOG_DBG << "   setting attrib " << ar->getName() << " = " << v;
+					std::wstring wn = utils::toUTF16FromOSNarrow(ar->getName());
+					mAttributeSource->setFloat(wn.c_str(), v);
+					setAttributes.set(ai);
+				} else if (ar.isString()) {
+					// TODO
+					setAttributes.set(ai);
+				}
 			}
-			setAttributes.reset(ai);
-			ai = setAttributes.find_next(ai);
 		}
 	}
-
-	if (DBG) LOG_DBG << "initial shape geo from group " << group->getName() << ": vtx = " << vtx << ", idx = " << idx << ", faceCounts = " << faceCounts;
 
 	const prt::AttributeMap* initialShapeAttrs = mAttributeSource->createAttributeMap();
 
@@ -427,7 +443,6 @@ OP_ERROR SOP_PRT::cookMySop(OP_Context &context) {
 
 		HoudiniGeometry hg(gdp);
 		{
-			LOG_DBG << "generating... ";
 			prt::Status stat = prt::generate(
 					&isc.mInitialShapes[0], isc.mInitialShapes.size(), 0,
 					&mAllEncoders[0], mAllEncoders.size(), &mAllEncoderOptions[0],
@@ -436,7 +451,6 @@ OP_ERROR SOP_PRT::cookMySop(OP_Context &context) {
 			if(stat != prt::STATUS_OK) {
 				LOG_ERR << "prt::generate() failed with status: '" << prt::getStatusDescription(stat) << "' (" << stat << ")";
 			}
-			LOG_DBG << "done.";
 		}
 
 		select(GU_SPrimitive);
@@ -446,105 +460,37 @@ OP_ERROR SOP_PRT::cookMySop(OP_Context &context) {
 	return error();
 }
 
-void getParamDef(const prt::RuleFileInfo* info, std::vector<std::string>& createdParams, std::ostream& defStream) {
+namespace {
+
+void getParamDef(
+		const prt::RuleFileInfo* info,
+		std::vector<std::string>& createdParams,
+		std::ostream& defStream
+) {
 	for(size_t i = 0; i < info->getNumAttributes(); i++) {
-		if(info->getAttribute(i)->getNumParameters() != 0) continue;
+		if (info->getAttribute(i)->getNumParameters() != 0)
+			continue;
+
 		const wchar_t* attrName = info->getAttribute(i)->getName();
 		std::string nAttrName = utils::toOSNarrowFromUTF16(attrName);
 		nAttrName = nAttrName.substr(8); // TODO: better way to remove style for now...
-		LOG_DBG << "rule attr: " << nAttrName;
-
-//		parmDef << "parm {\n";
-//		parmDef << "name    \"FloorHeight\"\n";
-//		parmDef << "label   \"\"\n";
-//		parmDef << "type    float\n";
-//		//parmDef << "invisible\n";
-//		parmDef << "size    1\n";
-//		parmDef << "default { 5 }\n";
-//		parmDef << "range   { 0 10 }\n";
-//		parmDef << "export  none\n";
-//		parmDef << "}\n";
-
 
 		defStream << "parm {" << "\n";
 		defStream << "    name  \"" << nAttrName << "\"" << "\n";
-		defStream << "    label \"\"\n";
-
+		defStream << "    label \"" << nAttrName << "\"\n";
 
 		switch(info->getAttribute(i)->getReturnType()) {
 		case prt::AAT_BOOL: {
-			//				for(size_t a = 0; a < info->getAttribute(i)->getNumAnnotations(); a++) {
-			//					const prt::Annotation* an = info->getAttribute(i)->getAnnotation(a);
-			//					if(!(wcscmp(an->getName(), ANNOT_RANGE)))
-			//						e = new PRTEnum(prtNode, an);
-			//				}
-			//
-
 			defStream << "    type    integer\n";
 			break;
 		}
 		case prt::AAT_FLOAT: {
-			//			double min = std::numeric_limits<double>::quiet_NaN();
-			//			double max = std::numeric_limits<double>::quiet_NaN();
-			//			for(size_t a = 0; a < info->getAttribute(i)->getNumAnnotations(); a++) {
-			//				const prt::Annotation* an = info->getAttribute(i)->getAnnotation(a);
-			//				if(!(wcscmp(an->getName(), ANNOT_RANGE))) {
-			//					if(an->getNumArguments() == 2 && an->getArgument(0)->getType() == prt::AAT_FLOAT && an->getArgument(1)->getType() == prt::AAT_FLOAT) {
-			//						min = an->getArgument(0)->getFloat();
-			//						max = an->getArgument(1)->getFloat();
-			//					} else
-			//						e = new PRTEnum(prtNode, an);
-			//				}
-			//			}
-
-			createdParams.push_back(nAttrName);
-
-
 			defStream << "    type    float\n";
+			// TODO: handle @RANGE annotation:	parmDef << "range   { 0 10 }\n";
+			createdParams.push_back(nAttrName); // FIXME
 			break;
 		}
 		case prt::AAT_STR: {
-			//			MString exts;
-			//			bool    asFile  = false;
-			//			bool    asColor = false;
-			//			for(size_t a = 0; a < info->getAttribute(i)->getNumAnnotations(); a++) {
-			//				const prt::Annotation* an = info->getAttribute(i)->getAnnotation(a);
-			//				if(!(wcscmp(an->getName(), ANNOT_RANGE)))
-			//					e = new PRTEnum(prtNode, an);
-			//				else if(!(wcscmp(an->getName(), ANNOT_COLOR)))
-			//					asColor = true;
-			//				else if(!(wcscmp(an->getName(), ANNOT_DIR))) {
-			//					exts = MString(an->getName());
-			//					asFile = true;
-			//				} else if(!(wcscmp(an->getName(), ANNOT_FILE))) {
-			//					asFile = true;
-			//					for(size_t arg = 0; arg < an->getNumArguments(); arg++) {
-			//						if(an->getArgument(arg)->getType() == prt::AAT_STR) {
-			//							exts += MString(an->getArgument(arg)->getStr());
-			//							exts += " (*.";
-			//							exts += MString(an->getArgument(arg)->getStr());
-			//							exts += ");;";
-			//						}
-			//					}
-			//					exts += "All Files (*.*)";
-			//				}
-			//			}
-			//
-			//			std::wstring value = evalAttrs.find(name.asWChar())->second.mString;
-			//			MString mvalue(value.c_str());
-			//			if(!(asColor) && mvalue.length() == 7 && value[0] == L'#')
-			//				asColor = true;
-			//
-			//			if(e) {
-			//				MCHECK(addEnumParameter(node, attr, name, mvalue, e));
-			//			} else if(asFile) {
-			//				MCHECK(addFileParameter(node, attr, name, mvalue, exts));
-			//			} else if(asColor) {
-			//				MCHECK(addColorParameter(node, attr, name, mvalue));
-			//			} else {
-			//				MCHECK(addStrParameter(node, attr, name, mvalue));
-			//			}
-
 			defStream << "    type    string\n";
 			break;
 		}
@@ -553,11 +499,20 @@ void getParamDef(const prt::RuleFileInfo* info, std::vector<std::string>& create
 		}
 
 		defStream << "    size    1\n";
-//		parmDef << "default { 5 }\n";
-//		parmDef << "range   { 0 10 }\n";
 		defStream << "    export  none\n";
 		defStream << "}\n";
 	}
+}
+
+namespace UnitQuad {
+const double 	vertices[]				= { 0, 0, 0,  0, 0, 1,  1, 0, 1,  1, 0, 0 };
+const size_t 	vertexCount				= 12;
+const uint32_t	indices[]				= { 0, 1, 2, 3 };
+const size_t 	indexCount				= 4;
+const uint32_t 	faceCounts[]			= { 4 };
+const size_t 	faceCountsCount			= 1;
+}
+
 }
 
 bool SOP_PRT::handleParams(OP_Context &context) {
@@ -575,6 +530,22 @@ bool SOP_PRT::handleParams(OP_Context &context) {
 		LOG_ERR << "rule file is empty/invalid, cannot continue";
 		return false;
 	}
+
+	// -- start rule
+	// TODO: search for @StartRule
+	// Default$Footprint
+	UT_String utStyle;
+	evalString(utStyle, NODE_PARAM_STYLE, 0, now);
+	mStyle = utils::toUTF16FromOSNarrow(utStyle.toStdString());
+
+	// -- start rule
+	// TODO: search for @StartRule
+	// Default$Footprint
+	UT_String utStartRule;
+	evalString(utStartRule, NODE_PARAM_START_RULE, 0, now);
+	mStartRule = utils::toUTF16FromOSNarrow(utStartRule.toStdString());
+
+	LOG_DBG << L"'style = " << mStyle << L", start rule = " << mStartRule;
 
 	// -- rule package
 	UT_String utNextRPKStr;
@@ -616,10 +587,50 @@ bool SOP_PRT::handleParams(OP_Context &context) {
 			const prt::RuleFileInfo* info = prt::createRuleFileInfo(cgbURI, 0, &status);
 			if (status == prt::STATUS_OK) {
 
-				mActiveParams.clear();;
+				// eval rule attribute values
+				prt::AttributeMapBuilder* amb = prt::AttributeMapBuilder::create();
+				{
+					HoudiniGeometry hg(nullptr, amb);
+					const prt::AttributeMap* emptyAttrMap = amb->createAttributeMapAndReset();
+
+					prt::InitialShapeBuilder* isb = prt::InitialShapeBuilder::create();
+					isb->setGeometry(UnitQuad::vertices, UnitQuad::vertexCount, UnitQuad::indices, UnitQuad::indexCount, UnitQuad::faceCounts, UnitQuad::faceCountsCount);
+					std::wstring startRule = mStyle + L"$" + mStartRule;
+					isb->setAttributes(mRuleFile.c_str(), startRule.c_str(), 666, L"temp", emptyAttrMap, mAssetsMap);
+
+					prt::Status status = prt::STATUS_UNSPECIFIED_ERROR;
+					const prt::InitialShape* is = isb->createInitialShapeAndReset(&status);
+					isb->destroy();
+					const prt::InitialShape* iss[1] = { is };
+
+					const prt::EncoderInfo* encInfo = prt::createEncoderInfo(ENCODER_ID_CGA_EVALATTR);
+					const prt::AttributeMap* encOpts = nullptr;
+					encInfo->createValidatedOptionsAndStates(nullptr, &encOpts);
+					encInfo->destroy();
+
+					const wchar_t* encs[1] = { ENCODER_ID_CGA_EVALATTR };
+					const prt::AttributeMap* encsOpts[1] = { encOpts };
+
+					prt::Status stat = prt::generate(iss, 1, nullptr, encs, 1, encsOpts, &hg, mPRTCache, nullptr, nullptr);
+					if(stat != prt::STATUS_OK) {
+						LOG_ERR << "prt::generate() failed with status: '" << prt::getStatusDescription(stat) << "' (" << stat << ")";
+					}
+
+					encOpts->destroy();
+					is->destroy();
+					emptyAttrMap->destroy();
+				}
+
+				const prt::AttributeMap* defAttrVals = amb->createAttributeMap();
+				amb->destroy();
+				LOG_DBG << "defAttrVals = " << utils::objectToXML(defAttrVals);
+
+				// build spare param definition
+				mActiveParams.clear();
 				std::ostringstream paramDef;
 				getParamDef(info, mActiveParams, paramDef);
 				std::string s = paramDef.str();
+				LOG_DBG << " INPUT PARMS" << s;
 
 				OP_Director* director = OPgetDirector();
 				director->deleteAllNodeSpareParms(this);
@@ -628,38 +639,47 @@ bool SOP_PRT::handleParams(OP_Context &context) {
 				UT_String errors;
 				director->loadNodeSpareParms(this, istr, errors);
 
+				// setup spare params with rule defaults
+				size_t keyCount = 0;
+				const wchar_t* const* cKeys = defAttrVals->getKeys(&keyCount);
+				for (size_t k = 0; k < keyCount; k++) {
+					const wchar_t* key = cKeys[k];
+					std::string nKey = utils::toOSNarrowFromUTF16(key);
+					nKey = nKey.substr(8); // TODO: better way to remove style for now...
+					switch (defAttrVals->getType(key)) {
+					case prt::AttributeMap::PT_FLOAT: {
+						setFloat(nKey.c_str(), 0, 0.0, defAttrVals->getFloat(key));
+						break;
+					}
+					// TODO: other types
+					}
+				}
+
+#if 0
 				std::ostringstream ostr;
 				director->saveNodeSpareParms(this, true, ostr);
-				LOG_DBG << ostr.str();
+				LOG_DBG << " SAVED PARMS" << ostr.str();
+#endif
+
+				defAttrVals->destroy();
 				info->destroy();
 			}
+			else {
+				LOG_ERR << "failed to get rule file info";
+				return false;
+			}
 
-			// new rpk is "go"
+			// new rpk is ready
 			mRPKURI = nextRPKURI;
 		}
 	}
-
-	// -- start rule
-	// TODO: search for @StartRule
-	// Default$Footprint
-	UT_String utStyle;
-	evalString(utStyle, NODE_PARAM_STYLE, 0, now);
-	mStyle = utils::toUTF16FromOSNarrow(utStyle.toStdString());
-
-	// -- start rule
-	// TODO: search for @StartRule
-	// Default$Footprint
-	UT_String utStartRule;
-	evalString(utStartRule, NODE_PARAM_START_RULE, 0, now);
-	mStartRule = utils::toUTF16FromOSNarrow(utStartRule.toStdString());
-
-	LOG_DBG << L"'style = " << mStyle << L", start rule = " << mStartRule;
 
 	return true;
 }
 
 bool SOP_PRT::updateParmsFlags() {
 	for (std::string& p: mActiveParams) {
+		// TODO: other types than float
 		double v = evalFloat(p.c_str(), 0, 0); // TODO: time
 		std::wstring wp = utils::toUTF16FromOSNarrow(p);
 		mAttributeSource->setFloat(wp.c_str(), v);
