ba6514b35a0cf3c48fca88d5dee08a455f801e83
Simon Haegler <shaegler@esri.com>
2019-01-10 12:00:27 +0100

    ci: pruning houdini conan package (size optimization)

--- conan/houdini/conanfile.py
+++ conan/houdini/conanfile.py
@@ -19,14 +19,16 @@ class HoudiniConan(ConanFile):
             local_install = os.getenv('HOUDINI_INSTALL')\
                 if 'HOUDINI_INSTALL' in os.environ\
                 else self.houdiniDefaultInstallationPath.format(self.version)
-            self.copy("*", ".", local_install)
+            #self.copy("*", ".", local_install, excludes=("bin/*","python27/*","engine/*","qt/*"))
+            self.copy("custom/*", ".", local_install)
+            self.copy("toolkit/*", ".", local_install)
         elif self.settings.os == "Linux":
             local_install = os.getenv('HOUDINI_INSTALL')\
                 if 'HOUDINI_INSTALL' in os.environ\
                 else "/opt/hfs{}".format(self.version)
-            # the python exclude is a workaround: houdini installs some
-            # files in the python subdir as only readable by root
-            self.copy("*", ".", local_install, symlinks=True, excludes="python/*")
+            #self.copy("*", ".", local_install, symlinks=True, excludes=("python/*","engine/*","qt/*"))
+            self.copy("dsolib/*", ".", local_install)
+            self.copy("toolkit/*", ".", local_install)
         elif self.settings.os == "Macos":
             # TODO
             pass
