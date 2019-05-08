d173fdd8ef85120579fd9a72f95be40f46c70c03
Simon Haegler <shaegler@esri.com>
2019-05-08 10:44:37 +0200

    Updated file preambles

--- src/palladio/LRUCache.h
+++ src/palladio/LRUCache.h
@@ -134,7 +134,7 @@ private:
 
 
 /*
- * Copyright 2014-2018 Esri R&D Zurich and VRBN
+ * Copyright 2014-2019 Esri R&D Zurich and VRBN
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
