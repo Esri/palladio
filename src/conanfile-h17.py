be3e7bcd7376689cf77c02a731691b8dbd8182bf
Simon Haegler <shaegler@esri.com>
2020-06-14 19:33:47 +0200

    conan: updated required CE SDK (PRT) version to 2.2.6332
    CityEngine 2020.0

--- src/conanfile-h17.py
+++ src/conanfile-h17.py
@@ -18,5 +18,5 @@ class PalladioConan(ConanFile):
             if "PLD_CONAN_CESDK_VERSION" in os.environ:
                 cesdk_version = os.environ["PLD_CONAN_CESDK_VERSION"]
             else:
-                cesdk_version = "2.1.5704"
+                cesdk_version = "2.2.6332"
             self.requires("cesdk/{}@esri-rd-zurich/stable".format(cesdk_version))
