bbaf0c34c8f0ee99d07dc2803fd9c79e7b637a74
Simon Haegler <shaegler@esri.com>
2020-11-15 12:43:23 +0100

    Also update Houdini 18.5 profile to CE SDK 2.3 (CityEngine 2020.1)

--- src/conanfile-h185.py
+++ src/conanfile-h185.py
@@ -18,5 +18,5 @@ class PalladioConan(ConanFile):
             if "PLD_CONAN_CESDK_VERSION" in os.environ:
                 cesdk_version = os.environ["PLD_CONAN_CESDK_VERSION"]
             else:
-                cesdk_version = "2.2.6332"
+                cesdk_version = "2.3.6821"
             self.requires("cesdk/{}@esri-rd-zurich/stable".format(cesdk_version))
